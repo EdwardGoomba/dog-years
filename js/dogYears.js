a0d0293f5ec75adb765bc718da41785c3093c41f
EdwardGoomba <ask@edwardvdesign.com>
2017-08-18 23:04:00 -0400

    Added js code to convert human to dog years

---
+++ js/dogYears.js
@@ -0,0 +1,9 @@
+var myAge = 27;
+
+var firstYears = 2 * 10.5;
+
+var laterYears = (myAge - 2) * 4;
+
+var myAgeInDogYears = firstYears + laterYears;
+
+console.log('My age in dog years is ' + myAgeInDogYears);
